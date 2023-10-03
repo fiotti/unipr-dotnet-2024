e569f74d6d5251d7ce848aeb08bee2ff7fa5639d
Fabio Iotti <fabio.iotti@alad.cloud>
2023-10-03 15:32:59 +0200

    Reflection

---
+++ 16_Reflection/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// In C# è possibile fare uso di "reflection" per verificare a runtime qual'è
+// la struttura di un tipo (classe, struct, ecc...) o di un assembly.
+
+void PrintDefinitionOf(Type type)
+{
+    MemberInfo[] members = type
+        .GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+    Console.WriteLine($"Members of {type.Name}:");
+
+    foreach (MemberInfo member in members)
+    {
+        bool isGenerated = member.GetCustomAttribute<CompilerGeneratedAttribute>() != null;
+
+        Console.Write($"- {member.MemberType}: {member.Name}");
+        if (isGenerated)
+            Console.Write(" (generated)");
+
+        Console.WriteLine();
+    }
+}
+
+Type myClassType = typeof(MyClass);
+PrintDefinitionOf(myClassType);
+
+// Output:
+// Members of MyClass:
+// - Method: get_MyStaticProperty (generated)
+// - Method: set_MyStaticProperty (generated)
+// - Method: get_MyProperty (generated)
+// - Method: set_MyProperty (generated)
+// - Method: get_MyProtectedProperty (generated)
+// - Method: set_MyProtectedProperty (generated)
+// - Constructor: .ctor
+// - Property: MyStaticProperty
+// - Property: MyProperty
+// - Property: MyProtectedProperty
+// - Field: MyField
+// - Field: _myPrivateField
+// - Field: <MyProperty>k__BackingField (generated)
+// - Field: <MyProtectedProperty>k__BackingField (generated)
+// - Field: <MyStaticProperty>k__BackingField (generated)
+// - Field: MyConst
+
+
+
+// È possibile fare uso di reflection anche per leggere i valori a runtime,
+// bypassando eventuali controlli sui modificatori di accessibilità.
+
+void PrintValuesOf(object obj)
+{
+    Type type = obj.GetType();
+
+    MemberInfo[] members = type
+        .GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
+
+    Console.WriteLine($"Values of object of type {type.Name}:");
+
+    foreach (MemberInfo member in members)
+    {
+        if (member is PropertyInfo property)
+        {
+            Console.WriteLine($"- {property.Name}: {property.GetValue(obj)}");
+        }
+        else if (member is FieldInfo field)
+        {
+            Console.WriteLine($"- {field.Name}: {field.GetValue(obj)}");
+        }
+    }
+}
+
+MyClass.MyStaticProperty = 123;
+MyClass myObject = new("1", 2, 3f, 4.0);
+PrintValuesOf(myObject);
+
+// Output:
+// Values of object of type MyClass:
+// - MyStaticProperty: 123
+// - MyProperty: 2
+// - MyProtectedProperty: 4
+// - MyField: 1
+// - _myPrivateField: 3
+// - <MyProperty>k__BackingField: 2
+// - <MyProtectedProperty>k__BackingField: 4
+// - <MyStaticProperty>k__BackingField: 123
+// - MyConst: 42
+
+
+
+public class MyClass
+{
+    public const int MyConst = 42;
+
+    public static int MyStaticProperty { get; set; }
+
+    public string MyField;
+
+    private float _myPrivateField;
+
+    public MyClass(string field, int property, float privateField, double privateProperty)
+    {
+        MyField = field;
+        MyProperty = property;
+        _myPrivateField = privateField;
+        MyProtectedProperty = privateProperty;
+    }
+
+    public int MyProperty { get; set; }
+
+    protected double MyProtectedProperty { get; set; }
+}
---
+++ 17_Attributes/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Reflection;
+using System.Text;
+
+// Gli attributi permettono di aggiungere dei metadati al codice.
+//
+// Per ulteriori informazioni:
+// https://learn.microsoft.com/dotnet/csharp/advanced-topics/reflection-and-attributes/
+
+MyClass myObject = new("hi", 123);
+string serialized = MySerializer.Serialize(myObject);
+Console.WriteLine($"Serialized: {serialized}");
+
+// Output:
+// Serialized: [World: 123, Hello: hi]
+
+
+
+[SerializableWithMySerializer]
+public class MyClass
+{
+    [MySerializerName("Hello")]
+    private string _hello;
+
+    public MyClass(string hello, int worldValue)
+    {
+        _hello = hello;
+        WorldValue = worldValue;
+    }
+
+    [MySerializerName("World")]
+    public int WorldValue { get; }
+}
+
+
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+class SerializableWithMySerializerAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+class MySerializerNameAttribute : Attribute
+{
+    readonly string _name;
+
+    public MySerializerNameAttribute(string name)
+    {
+        _name = name;
+    }
+
+    public string Name => _name;
+}
+
+
+
+static class MySerializer
+{
+    public static string Serialize<T>(T obj)
+    {
+        bool isSerializable = typeof(T).GetCustomAttribute<SerializableWithMySerializerAttribute>() != null;
+        if (!isSerializable)
+            throw new ArgumentException($"{typeof(T).Name} not serializable with my serializer.");
+
+        MemberInfo[] members = typeof(T)
+            .GetMembers(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+        
+        StringBuilder sb = new();
+
+        sb.Append('[');
+
+        bool isFirst = true;
+        foreach (MemberInfo member in members)
+        {
+            MySerializerNameAttribute? attribute = member.GetCustomAttribute<MySerializerNameAttribute>();
+            if (attribute == null)
+                continue;
+
+            if (isFirst)
+                isFirst = false;
+            else
+                sb.Append(", ");
+
+            sb.Append(attribute.Name);
+            sb.Append(": ");
+
+            object? memberValue = member switch
+            {
+                PropertyInfo p => p.GetValue(obj),
+                FieldInfo f => f.GetValue(obj),
+                _ => throw new NotSupportedException()
+            };
+
+            sb.Append(memberValue);
+        }
+
+        sb.Append(']');
+
+        return sb.ToString();
+    }
+}
